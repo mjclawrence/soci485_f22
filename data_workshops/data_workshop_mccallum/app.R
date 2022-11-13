5ddf1a5bca087b710fb23fcbd464a738d6994457
mjclawrence <32300703+mjclawrence@users.noreply.github.com>
2022-11-12 23:55:22 -0500

    minor edits

--- data_workshops/data_workshop_mccallum/app.R
+++ data_workshops/data_workshop_mccallum/app.R
@@ -4,28 +4,38 @@ library(weights)
 library(DT)
 library(pollster)
 
-mccallum <- read_csv("https://raw.githubusercontent.com/mjclawrence/soci485_f22/master/data/cces_08_20_rev.csv")
+mccallum <- read_csv("https://raw.githubusercontent.com/mjclawrence/soci485_f22/master/data/cces_08_20_threeparty_states.csv")
+mccallum_all <- read_csv("https://raw.githubusercontent.com/mjclawrence/soci485_f22/master/data/cces_08_20_threeparty_all.csv")
 
-mccallum <- mccallum |> 
-  pivot_longer(names_to = "union", values_to = "percent", starts_with("union")) |> 
-  mutate(percent = percent * 100)
+mccallum_all <- mccallum_all |> 
+  mutate(state = "All")
+
+mccallum <- bind_rows(mccallum_all, mccallum) |> 
+  pivot_longer(names_to = "party", values_to = "pct",
+               ends_with("proportion")) |> 
+  mutate(pct = pct * 100,
+         party = factor(party,
+                        levels = c("dem_proportion",
+                                   "rep_proportion",
+                                   "other_proportion"))) 
+
 
 # Define UI for application that pulls a city and characteristics
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Democratic Vote By Union Household Status"),
+    titlePanel("Presidential Vote By Union Household Status"),
 
     # Sidebar with a slider input for number of bins 
         sidebarPanel(
           selectInput(inputId = "state", #name of input
                              label = "Choose a State:", #label displayed in ui
                              choices = as.character(unique(mccallum$state)),
-                             selected = "Alabama"),
+                             selected = "All"),
     checkboxGroupInput(inputId = "year",
                        label = "Select Year(s)",
                        choices = unique(mccallum$year),
-                       selected = "2020")
+                       selected = c("2008", "2020"))
     ),
         # Show a plot of the generated distribution
         mainPanel(
@@ -52,9 +62,9 @@ server <- function(input, output) {
   
   output$year_plot <- renderPlot({
         plot1 <- mccallum_filter() |> 
-          ggplot(aes(x = as.factor(year), y = percent, fill = union)) +
-          geom_col(position = position_dodge()) +
-          theme(legend.position = "bottom")
+          ggplot(aes(x = as.factor(year), y = pct, color = party, group = party)) +
+          geom_point() + geom_line() + facet_wrap(~union_any) +
+          theme(legend.position = "bottom") + scale_color_manual(values = c("blue", "red", "darkgreen"))
           
           plot1
         })
