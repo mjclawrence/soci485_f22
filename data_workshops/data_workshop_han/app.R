f8d2560ee249aa5e420f94bcf0db5d1cbf1f635b
mjclawrence <32300703+mjclawrence@users.noreply.github.com>
2022-11-29 18:42:23 -0500

    Adds eviction rate, eviction share, and population share to descriptive table

--- data_workshops/data_workshop_han/app.R
+++ data_workshops/data_workshop_han/app.R
@@ -16,13 +16,39 @@ library(shinythemes)
 ## For now we can use the web versions
 
 han <- read_csv("https://raw.githubusercontent.com/mjclawrence/soci485_f22/master/data/han_data_complete_pop.csv")
+eviction_lab <- read_csv("https://raw.githubusercontent.com/mjclawrence/soci485_f22/master/data/eviction_socius.csv")
+
+eviction_lab <- eviction_lab |> 
+  select(zip, eviction_count, renter_hh_2019) |> 
+  rename(GEOID = zip)
+
+han <- left_join(han, eviction_lab)
 
 han <- han |> 
   relocate(xsite, .after = "NAME") |> 
   relocate(evict_pct, .after = "xsite") |> 
   relocate(population, .after = "xsite") |> 
   relocate(majority_race, .after = population) |> 
-  pivot_longer(names_to = "characteristic", values_to = "value", 7:22)
+  relocate(c(eviction_count, renter_hh_2019), .after = evict_pct) |> 
+  pivot_longer(names_to = "characteristic", values_to = "value", 9:24)
+
+han <- han|>
+  mutate(variable_name = ifelse(characteristic == "median_hh_income", "Median Household Income",
+                                ifelse(characteristic == "gini", "Gini Coefficient",
+                                       ifelse(characteristic == "prop_owner_occ", "Proportion Owner-Occupied Residences",
+                                              ifelse(characteristic == "prop_renter_occ", "Proportion Renter-Occupied Residences",
+                                                     ifelse(characteristic == "unemploymentrate", "Unemployment Rate",
+                                                            ifelse(characteristic == "singlerate", "Proportion Households Headed by Single Parent",
+                                                                   ifelse(characteristic == "rent_pct_incomeE", "Percent Monthly Income Spent on Rent",
+                                                                          ifelse(characteristic == "snap_pct", "Percent Households Receiving SNAP Benefits",
+                                                                                 ifelse(characteristic == "nonwhite_pct", "Percent Population Nonwhite",
+                                                                                        ifelse(characteristic == "white_pct", "Percent Population White",
+                                                                                               ifelse(characteristic == "black_pct", "Percent Population Black",
+                                                                                                      ifelse(characteristic == "hisp_pct", "Percent Population Hispanic",
+                                                                                                             ifelse(characteristic == "asian_pct", "Percent Population Asian",
+                                                                                                                    ifelse(characteristic == "other_pct", "Percent Population Other Race",
+                                                                                                                           ifelse(characteristic == "Less_than_15_minutes", "Proportion Population with Commute Less than 15 minutes",
+                                                                                                                                  "Proportion Population with College Degree or Higher"))))))))))))))))
 
 # The tab with iframe embeds will need urls.
 ## Most of these don't work as embeds but this is where
@@ -46,66 +72,66 @@ citysites <- bind_cols(unique(han$xsite), websites) |>
 # First part of the app is setting up the user interface
 
 ui <- fluidPage(
-
+  
   ## The theme selector lets you experiment with different themes
   shinythemes::themeSelector(),
   
   ### Once you decide on a theme, hashtag the previous line and edit the next line to use your theme
   #shinytheme(theme = "flatly"),
-
+  
   ## Application title
-    titlePanel("Eviction and Race"),
-
+  titlePanel("Eviction and Race"),
+  
   ## Sidebar has two inputs right now.
-    ### The first is where users choose a city. We'll use that selection as a variable called "city"
-    ### The second is where users choose one or more characteristics. We'll use that selection as a variable called "characteristic"
-
-        sidebarPanel(
-          selectizeInput(inputId = "city", #name of input
-                      label = "Choose a city:", #label displayed in ui
-                      choices = as.character(unique(han$xsite)),
-                      selected = "PHILADELPHIA"),
-          selectizeInput(inputId = "characteristic", #name of input
-                        label = "Select characteristic(s):", #label displayed in ui
-                        #choices = as.character(unique(han$characteristic)),
-                        selected = "nonwhite_pct",
-                        multiple = TRUE,
-                        options = list(plugins= list('remove_button')),
-                        list(
-                          `Demographic Measures` = list(
-                            "Pct Non White Residents" = "nonwhite_pct",
-                            "Pct Asian Residents" = "asian_pct",
-                            "Pct Black Residents" = "black_pct",
-                            "Pct Hispanic Residents" = "hisp_pct",
-                            "Pct Other Residents" = "other_pct",
-                            "Pct White Residents" = "white_pct"
-                          ),
-                          `Zip Code Economic Indicators` = list(
-                          "Gini Coefficient" = "gini",
-                          "Unemployment Rate" = "unemploymentrate",
-                          "Pct of Residents With College Degree or More" = "college_plus",
-                          "Pct of Residents With Commutes < 15 Minutes" = "Less_than_15_minutes"),
-                             `Household Economic Indicators` = list(
-                               "Median Household Income" = "median_hh_income",
-                               "Median Pct of Income Spent on Rent" = "rent_pct_incomeE",
-                               "Pct of Households With Single Parent" = "singlerate",
-                               "Pct of Households Receiving SNAP Benefits" = "snap_pct",
-                               "Pct of Housing Units Occupied By Renters" = "prop_renter_occ")))
-                        ), # close the sidebar panel
-        
+  ### The first is where users choose a city. We'll use that selection as a variable called "city"
+  ### The second is where users choose one or more characteristics. We'll use that selection as a variable called "characteristic"
+  
+  sidebarPanel(
+    selectizeInput(inputId = "city", #name of input
+                   label = "Choose a city:", #label displayed in ui
+                   choices = as.character(unique(han$xsite)),
+                   selected = "PHILADELPHIA"),
+    selectizeInput(inputId = "characteristic", #name of input
+                   label = "Select characteristic(s):", #label displayed in ui
+                   #choices = as.character(unique(han$characteristic)),
+                   selected = "nonwhite_pct",
+                   multiple = TRUE,
+                   options = list(plugins= list('remove_button')),
+                   list(
+                     `Demographic Measures` = list(
+                       "Pct Non White Residents" = "nonwhite_pct",
+                       "Pct Asian Residents" = "asian_pct",
+                       "Pct Black Residents" = "black_pct",
+                       "Pct Hispanic Residents" = "hisp_pct",
+                       "Pct Other Residents" = "other_pct",
+                       "Pct White Residents" = "white_pct"
+                     ),
+                     `Zip Code Economic Indicators` = list(
+                       "Gini Coefficient" = "gini",
+                       "Unemployment Rate" = "unemploymentrate",
+                       "Pct of Residents With College Degree or More" = "college_plus",
+                       "Pct of Residents With Commutes < 15 Minutes" = "Less_than_15_minutes"),
+                     `Household Economic Indicators` = list(
+                       "Median Household Income" = "median_hh_income",
+                       "Median Pct of Income Spent on Rent" = "rent_pct_incomeE",
+                       "Pct of Households With Single Parent" = "singlerate",
+                       "Pct of Households Receiving SNAP Benefits" = "snap_pct",
+                       "Pct of Housing Units Occupied By Renters" = "prop_renter_occ")))
+  ), # close the sidebar panel
+  
   ## Main panel has three tabs right now.
-    ### We express the outputs here by name, but we create them in the server section of this file
-        mainPanel(
-          navbarPage(
-            "Select Tab For Output",
-            tabPanel("Correlation(s) With Eviction", 
-                    plotOutput("eviction_scatterplot")),
-            tabPanel("Descriptives By Majority Race", 
-                    dataTableOutput("race_descriptives")),
-            tabPanel("iFrame Test",
-                      htmlOutput("frame"))
-        ) # close the navbarPage
-    ) # close the main panel
+  ### We express the outputs here by name, but we create them in the server section of this file
+  mainPanel(
+    navbarPage(
+      "Select Tab For Output",
+      tabPanel("Correlation(s) With Eviction", 
+               plotOutput("eviction_scatterplot")),
+      tabPanel("Descriptives By Majority Race", 
+               DT::dataTableOutput("race_descriptives")),
+      tabPanel("iFrame Test",
+               htmlOutput("frame"))
+    ) # close the navbarPage
+  ) # close the main panel
 ) # close the ui code
 
 # Define server logic
@@ -126,59 +152,88 @@ server <- function(input, output) {
       filter(xsite == input$city)
   })
   
-## Here I am renaming each dataset after filtering
-### The debounce function adds a small amount of time (500ms) between filtering the datasets and creating the output.
-### Adding that time tends to make the output more responsive to the filters 
-
+  ## Here I am renaming each dataset after filtering
+  ### The debounce function adds a small amount of time (500ms) between filtering the datasets and creating the output.
+  ### Adding that time tends to make the output more responsive to the filters 
+  
   han_filter <- dataInput |>  debounce(500)
   
   site_filter <- citysiteInput |> debounce(500)
   
-
+  
   ## This section creates the scatterplot.
   ### The name we give to the object - eviction_scatterplot - is the name we will use in the output for the tab in the ui
   output$eviction_scatterplot <- renderPlot({
-        plot1 <- han_filter() |> 
-          group_by(characteristic) |> 
-          ggplot(aes(x = value,
-                     y = evict_pct,
-                     color = majority_race)) +
-          geom_point(aes(size = 3)) + 
-          facet_wrap(~characteristic, scales = "free") +
-          theme(legend.position = "bottom") + guides(size = FALSE)
+    plot1 <- han_filter() |> 
+      group_by(characteristic) |> 
+      ggplot(aes(x = value,
+                 y = evict_pct,
+                 color = majority_race)) +
+      geom_point(aes(size = 3)) + 
+      facet_wrap(~characteristic, scales = "free") +
+      theme(legend.position = "bottom") + guides(size = "none")
     
-          plot1
-        })
-
+    plot1
+  })
+  
   ## This section creates the descriptives table  
-  output$race_descriptives <- renderDataTable({    
-      table1 <- han_filter() |> 
-        group_by(characteristic, majority_race) |> 
-        summarise(mean = round(wtd.mean(value, population, na.rm = TRUE),2)) |> 
-        pivot_wider(names_from = "majority_race", values_from = "mean") |> 
-        datatable(rownames = FALSE,
-        options = list(
-          dom = 'Btip', # see explanation below
-          buttons = list(list(extend = 'copy', title = NULL))))
-
-        table1
-      })
+  output$race_descriptives <- DT::renderDataTable({    
+    table1 <- han_filter() |> 
+      mutate(total_pop = sum(population),
+             total_eviction = sum(eviction_count)) |> 
+      group_by(majority_race) |> 
+      summarise("Eviction Rate" = round(wtd.mean(evict_pct, population, na.rm = TRUE),3),
+                "Proportion of All Evictions" = round((sum(eviction_count)/total_eviction),3),
+                "Proportion of Total Population" = round((sum(population)/total_pop),3)) |> 
+      distinct() |> 
+      pivot_longer(names_to = "variable_name", values_to = "mean", 2:4) |> 
+      pivot_wider(names_from = "majority_race", values_from = "mean") 
     
+    table2 <- han_filter() |> 
+      group_by(variable_name, majority_race) |> 
+      summarise(mean = round(wtd.mean(value, population, na.rm = TRUE),2)) |> 
+      pivot_wider(names_from = "majority_race", values_from = "mean")
 
-## This section creates the iframe embeds of the websites
+      table3 <- bind_rows(table1, table2) |> 
+        relocate(variable_name) |> 
+        datatable(rownames = FALSE,
+                  options = list(
+                    dom = 'Btip', # see explanation below
+                    buttons = list(list(extend = 'copy', title = NULL)))) |> 
+        formatStyle(
+          "variable_name",
+          target = 'row',
+          backgroundColor = styleEqual(c("Eviction Rate", "BLACK"), 
+                                       c('cornflowerblue', 'white'))) |> 
+        formatStyle(
+          "variable_name",
+          target = 'row',
+          backgroundColor = styleEqual(c("Proportion of All Evictions", "WHITE"), 
+                                       c('cornflowerblue', 'white'))) |> 
+        formatStyle(
+          "variable_name",
+          target = 'row',
+          backgroundColor = styleEqual(c("Proportion of Population", "NO MAJORITY"), 
+                                       c('cornflowerblue', 'white')))
+          
+    table3
+  })
+  
+  
+  ## This section creates the iframe embeds of the websites
   output$frame <- renderUI({ 
     
     city_embed <- site_filter()$website
     
     tags$iframe(#src = 'https://datawrapper.de', 
-                src = city_embed,
-                width = "100%", height = "600",
-                seamless = TRUE)
+      src = city_embed,
+      width = "100%", height = "600",
+      seamless = TRUE)
   }) 
   
 } # This curly bracket closes the server code
 
 
 
- # After the ui code and server code are set, run the application 
+# After the ui code and server code are set, run the application 
 shinyApp(ui = ui, server = server)
